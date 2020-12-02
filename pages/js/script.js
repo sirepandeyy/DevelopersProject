7e28c2c191d01a52cac2b77fb27f08ae6cbac340
Sushantdubey79 <sushantdubey7607@gmail.com>
2020-12-02 16:51:27 +0530

    created home page

---
+++ pages/js/script.js
@@ -0,0 +1,11 @@
+jQuery(window).on('load',function() {
+	
+	/*
+	    Stop carousel
+	*/
+    $('#inam').carousel('pause');
+	$('#imam').carousel('pause');
+	$('#ipam').carousel('pause');
+    
+	
+});
